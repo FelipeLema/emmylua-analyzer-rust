a6890df9da6b3048f9db002816fffc61572a125e
Felipe Lema <felipelema@mortemale.org>
2025-03-14 13:54:39 -0300

    break reading main ws folder into separate fn

--- crates/emmylua_ls/src/handlers/initialized/client_config/neovim_config.rs
+++ crates/emmylua_ls/src/handlers/initialized/client_config/neovim_config.rs
@@ -5,25 +5,30 @@ use serde_json::Value;
 
 use crate::{context::ServerContextSnapshot, util::time_cancel_token};
 use emmylua_code_analysis::file_path_to_uri;
+use lsp_types::Uri;
 
 use super::ClientConfig;
 
-pub async fn get_client_config_neovim(
-    context: &ServerContextSnapshot,
-    config: &mut ClientConfig,
-) -> Option<()> {
+async fn scope_uri_from_context(context: &ServerContextSnapshot) -> Option<Uri> {
     let workspace_folders = context
         .workspace_manager
         .read()
         .await
         .workspace_folders
         .clone();
-    let main_workspace_folder = workspace_folders.get(0);
+    let main_workspace_folder = workspace_folders.first()?;
+    let scope_uri = file_path_to_uri(main_workspace_folder);
+    scope_uri
+}
+
+pub async fn get_client_config_neovim(
+    context: &ServerContextSnapshot,
+    config: &mut ClientConfig,
+) -> Option<()> {
     let client = &context.client;
-    let scope_uri = main_workspace_folder.map(|p| file_path_to_uri(p).unwrap());
     let params = lsp_types::ConfigurationParams {
         items: vec![lsp_types::ConfigurationItem {
-            scope_uri: scope_uri,
+            scope_uri: scope_uri_from_context(context).await,
             section: Some("Lua".to_string()),
         }],
     };
